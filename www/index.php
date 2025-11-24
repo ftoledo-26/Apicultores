183c37fcbccaa21f62c63f4227eb47c2e4113863
Francisco Manuel Toledo Pérez <ftolper633@cpifpalanturing.es>
2025-11-24 19:32:54 +0100

    Inicio Proyecto

--- www/index.php
+++ www/index.php
@@ -1,10 +1,11 @@
 <?php
-require_once __DIR__ . '/Database.php';
+require_once __DIR__ . '/config/Database.php';
 $pdo = new Database();
 
 try {
     $connection = $pdo->getConnection();
     echo "Conexión exitosa a la base de datos.";
+
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
