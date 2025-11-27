37e70efaa37c65c29aa6d5627aa5b96962916b87
Francisco Manuel Toledo Pérez <ftolper633@cpifpalanturing.es>
2025-11-27 18:02:39 +0100

    while(true){print (a)}

---
+++ www/Controllers/usuariosController.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/../Models/UsuariosModels.php';
+class usuariosController{
+    private UsuariosModels $usuarios;
+
+    public function __construct(UsuariosModels $models){
+        $this->usuarios = $models;
+    }
+
+    public function GetUsusuarios():array{
+        return $this->usuarios->obtenerTodos();
+    }
+}
+
+?>
