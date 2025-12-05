b81fbdfe475ee5a6ab2d215c3791c6652d4984cf
José María Caracuel Menacho <jcarmen0312@cpifpalanturing.es>
2025-12-05 16:36:57 +0100

    pull main

--- www/api/usuarios.php
+++ www/api/usuarios.php
@@ -4,10 +4,12 @@ header("Content-Type: application/json; charset=utf-8");
 require_once __DIR__ . "/../config/config.php";
 require_once __DIR__ . "/../config/Database.php";
 require_once __DIR__ . "/../Models/usuariosModels.php";
-
-
-
 require_once __DIR__ . "/../Controllers/usuariosController.php";
+require_once __DIR__ . "/../vendor/autoload.php";
+include __DIR__ . "/../middleware/auth.php";
+
+$datosToken = verificarToken();
+use Firebase\JWT\JWT;
 
 $db = Database::getConnection();
 $Usuario = new UsuariosModels($db);
@@ -15,7 +17,7 @@ $ControllerUser = new usuariosController($Usuario);
 
 $metodo = $_SERVER["REQUEST_METHOD"];
 $id = isset($_GET["id"]) ? (int) $_GET["id"] : null;
-$relacion = isset($_GET["include"]) ? (string) $_GET["include"] : null;
+
 
 switch ($metodo) {
     case "GET":
@@ -44,7 +46,7 @@ switch ($metodo) {
 
         break;
     case "PUT":
-
+        
         if ($id !== null) {
 
             $usuario = $ControllerUser->GetUsuarioById($id);
@@ -76,11 +78,25 @@ switch ($metodo) {
             }
         break;
     case "POST":
-        $input = json_decode(file_get_contents("php://input"), true) ?? [];
-        echo json_encode($ControllerUser->crear($input));
-        break;
+        $data = json_decode(file_get_contents("php://input"), true);
+
+        if ($data) {
+            $id = $ControllerUser->crear($data);
+
+            $payload = [
+                "id"    => $id,
+                "email" => $data['email'],
+                "rol"   => 'administrador',
+                "iat"   => time(),
+                "exp"   => time() + 3600
+            ];
+            $token = JWT::encode($payload, JWT_SECRET, 'HS256');
+            echo json_encode(["token" => $token]);
+        } else {
+            http_response_code(400);
+            echo json_encode(["error" => "Faltan datos obligatorios"]);
+        }
     break;
-    default:
         http_response_code(405);
         echo json_encode(["error" => "Solo GET, POST, PUT y DELETE"]);
         break;
